f3acfba1e9034ae7d2e9d95f1c7d0e0c8c518165
Franklin Smith <franklinsmith@apple.com>
2020-12-19 20:58:31 -0500

    updating

--- Allied Systems Northwest/Allied Systems Northwest/AppDelegate.swift
+++ Allied Systems Northwest/Allied Systems Northwest/AppDelegate.swift
@@ -15,8 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let homeViewController = EntryViewController()
+        //let homeViewController = EntryViewController()
+        let homeViewController = ViewController(nibName: "EntryViewController", bundle: nil)
         window?.rootViewController = homeViewController
+        window?.makeKeyAndVisible()
+        //print(homeViewController)
         return true
     }
 
