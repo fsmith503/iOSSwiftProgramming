0474b87c0dac9e2e95b8393b6eb7f10b23c9c588
Franklin Smith <franklinsmith@apple.com>
2020-12-26 10:17:50 -0800

    updating progress

---
+++ LootLogger/LootLogger/ItemStore.swift
@@ -0,0 +1,30 @@
+//
+//  ItemStore.swift
+//  LootLogger
+//
+//  Created by franklin smith on 12/26/20.
+//
+
+import UIKit
+
+class ItemStore{
+    
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5{
+            createItem()
+        }
+    }
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+    
+}
+
+
+
+
--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -9,4 +9,31 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+                return itemStore.allItems.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                             // create an instance of UITableViewCell with default appearance
+                            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+                                
+                            // set the text on the cell with the description of the item
+                            // that is at the nth index of items, where n = row this cell
+                            // will appear on the tableview
+                                
+                                let item = itemStore.allItems[indexPath.row]
+                                
+                                cell.textLabel?.text = item.name
+                                cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+                                
+                                return cell
+                                
+                             }
+    
 }
+
+
