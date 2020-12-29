723dea0523dea2ded075291d74168486c30d0fd7
franklin smith <franklinsmith@franklins-iMac-Pro.local>
2020-12-28 23:45:58 -0800

    trying to update outlets

---
+++ LootLogger/LootLogger/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by franklin smith on 12/28/20.
+//
+
+import UIKit
+
+class itemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
