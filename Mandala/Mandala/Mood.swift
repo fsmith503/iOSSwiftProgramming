fabe210c097f05aa2e61653acefaf745cdadc51c
franklin smith <franklinsmith@franklins-iPro.attlocal.net>
2021-01-04 06:53:48 -0800

    updating assets and struct files

---
+++ Mandala/Mandala/Mood.swift
@@ -0,0 +1,14 @@
+//
+//  Mood.swift
+//  Mandala
+//
+//  Created by franklin smith on 1/4/21.
+//
+
+import UIKit
+
+struct Mood {
+    var name: String
+    var image: UIImage
+    var color: UIColor
+}
---
+++ Mandala/Mandala/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by franklin smith on 1/4/21.
+//
+
+import Foundation
+
+struct MoodEntry{
+    var mood: Mood
+    var timeStamp: Date
+}
