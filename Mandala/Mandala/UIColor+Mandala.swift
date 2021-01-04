d14d82617d89dd1916a3079417504d5f89b16ddc
franklin smith <franklinsmith@franklins-iPro.attlocal.net>
2021-01-04 07:37:07 -0800

    idk

---
+++ Mandala/Mandala/UIColor+Mandala.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Mandala.swift
+//  Mandala
+//
+//  Created by franklin smith on 1/4/21.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
+    
+    
+}
