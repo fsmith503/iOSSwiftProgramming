6f89f94b9967a01d1bcbf1e97f9038faa8d4eb0b
franklin smith <franklinsmith@franklins-iMac-Pro.local>
2021-01-09 17:10:15 -0800

    updating outlets

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by franklin smith on 1/9/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    
+    
+    
+    func update(displaying image: UIImage?){
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    
+}
